8da925c8ec03405598d0db1f21af209c03860ecf
CJVDI\s-oscar1 <oscar@neoplanz.com>
2018-11-14 11:42:39 +0900

    freeCommit #2

---
+++ src/main/java/StringToMap/stringToMap.java
@@ -0,0 +1,42 @@
+/**
+ * https://www.mkyong.com/java/how-to-convert-java-map-to-from-json-jackson/
+ */
+package StringToMap;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import sun.rmi.runtime.Log;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class stringToMap {
+    public static void main(String[] agrs) throws IOException {
+        String param = "{\"channel\":\"11111\",\"info\":\"someInfo\",\"name\":\"anormaly\"}";
+        try {
+
+
+            String json =param;
+
+            Map<String, Object> map = new HashMap<String, Object>();
+
+            // convert JSON string to Map
+            ObjectMapper mapper = new ObjectMapper();
+            map = mapper.readValue(json, new TypeReference<Map<String, String>>(){});
+
+            System.out.println(map);
+
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
